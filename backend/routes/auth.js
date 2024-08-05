0c7d1dbb54030d12b1764aa66b122a8363185574
Niraj Singh <58655279+Niraj00@users.noreply.github.com>
2024-08-05 15:32:07 +0530

    fixed req body error

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const User = require('../models/User');
 
 router.get('/', (req, res) => {
+    console.log(req.body);
     const user = User(req.body);
     user.save();
-    console.log(req.body);
-    res.send("user has been registered successfully");
+    res.send(req.body);
 })
 
 module.exports = router;
